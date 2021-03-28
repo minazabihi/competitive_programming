dacf8196ad6a581ae5e0520095caf0f1d10317dc
minazabihi <61754510+minazabihi@users.noreply.github.com>
2021-03-28 21:43:55 +0300

    add 957.cpp

---
+++ binary_search/957.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int y,n,v;
+    while(cin >> y){
+        cin >> n;
+        vector<int> list;
+        for(int i=0;i<n;i++){
+            cin >> v;
+            list.push_back(v);
+        }
+        int max=0, start=0, end=0;
+        for(int i=0;i<list.size();i++){
+            auto upper = upper_bound(list.begin()+i,list.end(),list[i]+y-1);
+            int numPopes = upper-(list.begin()+i);
+            if(numPopes > max){
+                max = numPopes;
+                start = list[i];
+                end = *(upper-1);
+            }
+        }
+        cout<<max<<" "<<start<<" "<<end<<endl;
+    }
+}
